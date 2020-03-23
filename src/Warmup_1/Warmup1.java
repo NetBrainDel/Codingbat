cdab7a387a3674b1521e0ac6fcd3116dfd7fc89b
NetBRAINdel <62387102+NetBRAINdel@users.noreply.github.com>
2020-03-23 16:02:23 +0300

    1

--- src/Warmup_1/Warmup1.java
+++ src/Warmup_1/Warmup1.java
@@ -67,6 +67,16 @@ public boolean makes10(int a, int b) {
         return false;
     }
 }
+///////////////////////////////////////////////////////////////////////////////№7
+public boolean nearHundred(int n) {
+    if(Math.abs(100 - n)<=10 || Math.abs(200 - n )<=10){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+////////////////////////////////////////////////////////////////////////////////№8
 
     }
 
