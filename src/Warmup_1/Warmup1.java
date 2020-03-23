4f34b2781bbbb96be0d18dac52625f6900cfea44
NetBRAINdel <62387102+NetBRAINdel@users.noreply.github.com>
2020-03-23 15:54:26 +0300

    1

--- src/Warmup_1/Warmup1.java
+++ src/Warmup_1/Warmup1.java
@@ -59,7 +59,14 @@ public boolean parrotTrouble(boolean talking, int hour)
 
 }
 ////////////////////////////////////////////////////////////////////////////////№6
-
+public boolean makes10(int a, int b) {
+    if(a == 10 || b == 10 || a + b == 10){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
 
     }
 
