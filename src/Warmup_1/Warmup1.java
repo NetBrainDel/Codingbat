3d2e4a5852037da456dd5675568d959c6e10b524
NetBRAINdel <62387102+NetBRAINdel@users.noreply.github.com>
2020-03-23 15:04:13 +0300

    1

--- src/Warmup_1/Warmup1.java
+++ src/Warmup_1/Warmup1.java
@@ -1,7 +1,39 @@
 package Warmup_1;
 
 public class Warmup1 {
-    public static void main(String[] args) {
-        System.out.println("egef");
+///////////////////////////////////////////////////////////////////////////////
+public boolean sleepIn(boolean weekday, boolean vacation) {
+    if(!weekday || vacation){
+        return true;
+    }
+    else{
+        return false;
     }
 }
+//////////////////////////////////////////////////////////////////////////////
+public boolean monkeyTrouble(boolean aSmile, boolean bSmile) {
+    if (aSmile && bSmile || !aSmile && !bSmile)
+    {//Trouble
+        return true;
+    }
+    else{
+    return false;
+    }
+}
+///////////////////////////////////////////////////////////////////////////////
+public int sumDouble(int a, int b) {
+    int sum = a + b;
+
+    if(a != b){
+        sum *=2;
+        return sum;
+    }
+    return sum;
+}
+////////////////////////////////////////////////////////////////////////////////////
+
+
+
+
+    }
+
