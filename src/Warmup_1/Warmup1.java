0310b2220a99fc978e73743fbc0c3e62da5fc329
NetBRAINdel <62387102+NetBRAINdel@users.noreply.github.com>
2020-03-24 14:56:52 +0300

    1

--- src/Warmup_1/Warmup1.java
+++ src/Warmup_1/Warmup1.java
@@ -89,6 +89,15 @@ public boolean posNeg(int a, int b, boolean negative) {
     return false;
 }
 ///////////////////////////////////////////////////////////////////////////////№9
+public String notString(String str) {
+    if (str.length() >= 3 && str.substring(0, 3).equals("not")) {
+        return str;
+    }else {
+
+        return "not " + str;
+    }
+}
+///////////////////////////////////////////////////////////////////////////////№10
 
     }
 
