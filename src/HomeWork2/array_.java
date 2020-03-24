04322a516ed5c73d91ff7d872fb7ae3587311aca
NetBRAINdel <62387102+NetBRAINdel@users.noreply.github.com>
2020-03-24 23:15:01 +0300

    array

--- src/HomeWork2/array_.java
+++ src/HomeWork2/array_.java
@@ -4,27 +4,52 @@ import java.util.Scanner;
 
 public class array_ {
     public static void main(String[] args) {
+///////////////////////////////////////////////for\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+
         int a;
-        int e;
+        //int e;
         System.out.println("введите размер массива");
-        Scanner q = new Scanner(System.in);
-        a = q.nextInt();
-        int array[] = new int[a];
+        Scanner in = new Scanner(System.in);
+        a = in.nextInt();
+        int array[] = new int[5];
 
-        for (int i = 0; i < a; i+=2) {
-            System.out.println("________________________");
-            System.out.println("введите значение массива");
-            Scanner w = new Scanner(System.in);
-            e = w.nextInt();
+       // for (int i = 0; i < a; i+=2) {
+          //  System.out.println("________________________");
+         //   System.out.println("введите значение массива");
+         //   Scanner w = new Scanner(System.in);
+         //   e = w.nextInt();
 
-            array[i] = e;
+        //   array[i] = e;
+
+        //    for (int j = 1; j <= 1; j++) {
+
+         //       System.out.println("элемент массива: "+i+" его значение: "+ array[i]);
+        //    }
+
+////////////////////////////////////////////foreach\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+
+                for (int i = 0; i < a; i++) {
+                    System.out.println("Введите значение массива: ");
+                    array[i] = in.nextInt();
+                    System.out.println();
+                    System.out.println("элемент массива и его значение: "+i+" : "+array[i]);
+                    System.out.println();
+                }
+                    System.out.println();
+                    System.out.println("значения массива: ");
+                 for (int t : array) {
+                    System.out.print("|"+t + "|");
+                }
 
 
-            for (int j = 1; j <= 1; j++) {
-
-                System.out.println("элемент массива: "+i+" его значение: "+ array[i]);
             }
+
+
         }
-    }
-}
+
+
+
+
+
+
 
