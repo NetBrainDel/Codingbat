460ea975d1369c9df6c4b304f35459b1437c565d
NetBRAINdel <62387102+NetBRAINdel@users.noreply.github.com>
2020-03-24 16:49:15 +0300

    1

--- src/Warmup_1/Warmup1.java
+++ src/Warmup_1/Warmup1.java
@@ -203,6 +203,128 @@ public boolean loneTeen(int a, int b) {
     return false;
 }
 ///////////////////////////////////////////////////////////////////////////////////№21
+public String delDel(String str) {
+    if (str.length()>=4 && str.substring(1, 4).equals("del")) {
+
+        return str.substring(0, 1) + str.substring(4);
+    }
+
+    return str;
+}
+/////////////////////////////////////////////////////////////////////////////////////№22
+public boolean mixStart(String str) {
+
+    if (str.length() < 3){
+        return false;
+    }
+    String two = str.substring(1, 3);
+
+    if (two.equals("ix")) {
+        return true;
+    }
+    else {
+        return false;
+    }
+
+}
+//////////////////////////////////////////////////////////////////////////////////////№23
+public String startOz(String str) {
+    String result = "";
+
+    if (str.length() >= 1 && str.charAt(0)=='o') {
+        result = result + str.charAt(0);
+    }
+
+    if (str.length() >= 2 && str.charAt(1)=='z') {
+        result = result + str.charAt(1);
+    }
+
+    return result;
+}
+///////////////////////////////////////////////////////////////////////////////////////////№24
+public int intMax(int a, int b, int c) {
+    int max;
+
+    if (a > b) {
+        max = a;
+    } else {
+        max = b;
+    }
+
+    if (c > max) {
+        max = c;
+    }
+
+    return max;
+
+}
+///////////////////////////////////////////////////////////////////////////////////////////№25
+public int close10(int a, int b) {
+    int a2 = Math.abs(a - 10);
+    int b2 = Math.abs(b - 10);
+
+    if (a2 < b2) {
+        return a;
+    }
+    else if (b2 < a2) {
+        return b;
+    }
+    return 0;
+
+}
+//////////////////////////////////////////////////////////////////////////////////////////////№26
+public boolean in3050(int a, int b) {
+    if((a >= 30 && a <= 40) && (b >= 30 && b <= 40)){
+        return true;
+    }
+    if((a >= 40 && a <= 50) && (b >= 40 && b <= 50)){
+        return true;
+    }
+    return false;
+}
+////////////////////////////////////////////////////////////////////////////////////////////////№27
+public int max1020(int a, int b) {
+    if (b > a) {
+        int a2 = a;
+        a = b;
+        b = a2;
+    }
+    if (a >= 10 && a <= 20){
+
+        return a;
+    }
+    if (b >= 10 && b <= 20){
+
+        return b;
+    }
+    else {
+
+        return 0;
+    }
+}
+////////////////////////////////////////////////////////////////////////////////////////////////////№28
+public boolean stringE(String str) {
+    int count = 0;
+
+    for (int i=0; i<str.length(); i++) {
+        if (str.charAt(i) == 'e'){
+            count++;
+        }
+    }
+    if(count >= 1 && count <= 3){
+        return true;
+    }
+
+    return false;
+}
+/////////////////////////////////////////////////////////////////////////////////////////////////№29
+public boolean lastDigit(int a, int b) {
+    if(a % 10 == b % 10){
+        return true;
+    }
+    return false;
+}
+/////////////////////////////////////////////////////////////////////////////////////////////////№30
 
 }
 
