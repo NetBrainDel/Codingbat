8a6b4e24b4735e0b731cb4a57112430f252ad217
NetBRAINdel <62387102+NetBRAINdel@users.noreply.github.com>
2020-03-24 16:52:39 +0300

    1

--- src/Warmup_1/Warmup1.java
+++ src/Warmup_1/Warmup1.java
@@ -325,6 +325,22 @@ public boolean lastDigit(int a, int b) {
     return false;
 }
 /////////////////////////////////////////////////////////////////////////////////////////////////№30
+public String endUp(String str) {
+    if (str.length() <= 3) return str.toUpperCase();
+    int cut = str.length() - 3;
+    String front = str.substring(0, cut);
+    String back = str.substring(cut);
 
+    return front + back.toUpperCase();
+}
+///////////////////////////////////////////////////////////////////////////////////////////////№31
+public String everyNth(String str, int n) {
+    String result = "";
+
+    for (int i=0; i<str.length(); i+=n) {
+        result = result + str.charAt(i);
+    }
+    return result;
+ }
 }
 
