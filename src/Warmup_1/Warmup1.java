ed2cdeca63374edd4b9c3dba43aa9019c9ed8067
NetBRAINdel <62387102+NetBRAINdel@users.noreply.github.com>
2020-03-24 16:53:28 +0300

    1

--- src/Warmup_1/Warmup1.java
+++ src/Warmup_1/Warmup1.java
@@ -338,7 +338,7 @@ public String everyNth(String str, int n) {
     String result = "";
 
     for (int i=0; i<str.length(); i+=n) {
-        result = result + str.charAt(i);
+        result += str.charAt(i);
     }
     return result;
  }
