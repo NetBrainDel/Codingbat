809d2f03df48b6299a32fcd21ea92369daff5b24
NetBRAINdel <62387102+NetBRAINdel@users.noreply.github.com>
2020-03-24 21:49:55 +0300

    array

---
+++ src/HomeWork2/array_.java
@@ -0,0 +1,28 @@
+package HomeWork2;
+
+import java.util.Scanner;
+
+public class array_ {
+    public static void main(String[] args) {
+        int a;
+        int e;
+        System.out.println("введите размер массива");
+        Scanner q = new Scanner(System.in);
+        a = q.nextInt();
+        int array[] = new int[a];
+
+        for (int i = 0; i < a; i++) {
+            System.out.println("________________________");
+            System.out.println("введите значение массива");
+            Scanner w = new Scanner(System.in);
+            e = w.nextInt();
+
+            array[i] = e;
+
+            for (int j = 1; j <= 1; j++) {
+                System.out.println("элемент массива: "+i+" его значение: "+ array[i]);
+            }
+        }
+    }
+}
+
