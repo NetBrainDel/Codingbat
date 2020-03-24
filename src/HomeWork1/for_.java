1632ac9ce48d5fa833151e35f9689a102a159b6f
NetBRAINdel <62387102+NetBRAINdel@users.noreply.github.com>
2020-03-24 23:22:02 +0300

    multiplication table

--- src/HomeWork1/for_.java
+++ src/HomeWork1/for_.java
@@ -4,7 +4,7 @@ public class for_ {
     public static void main(String[] args) {
         for (int i = 1; i <= 10 ; i++) {
             for (int j = 1; j <= 10; j++) {
-                System.out.printf("%5d", i*j);
+                System.out.printf("%4d", i*j);
             }
             System.out.println();
         }
